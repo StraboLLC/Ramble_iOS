44d8f94758417a1a3d56cbd9c44e5d373d5282f8
Nate Beatty <nate.beatty@gmail.com>
2012-01-17 16:48:48 -0500

    Initial Commit

---
+++ Ramble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ramble
+//
+//  Created by Thomas Beatty on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ramble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ramble
+//
+//  Created by Thomas Beatty on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
