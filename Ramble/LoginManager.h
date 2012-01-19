bfadf469df076d3d82b04ce52ce1301b93ab7179
Nate Beatty <nate.beatty@gmail.com>
2012-01-19 00:12:00 -0500

    Created a LoginManager

---
+++ Ramble/LoginManager.h
@@ -0,0 +1,13 @@
+//
+//  LoginManager.h
+//  Ramble
+//
+//  Created by Thomas Beatty on 1/18/12.
+//  Copyright (c) 2012 Strabo LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginManager : NSObject
+
+@end
---
+++ Ramble/LoginManager.m
@@ -0,0 +1,13 @@
+//
+//  LoginManager.m
+//  Ramble
+//
+//  Created by Thomas Beatty on 1/18/12.
+//  Copyright (c) 2012 Strabo LLC. All rights reserved.
+//
+
+#import "LoginManager.h"
+
+@implementation LoginManager
+
+@end
